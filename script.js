416ca5004511fa4c77f87e1e6e50d9cb0cf2248f
Md. Jamil Khan <jamil.mu.cse@gmail.com>
2025-06-28 07:37:14 +0600

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
