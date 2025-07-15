78ead54b5ca90e80631650616ffc2a40081bba18
Md. Jamil Khan <jamil.mu.cse@gmail.com>
2025-07-15 07:49:41 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Optional: Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
